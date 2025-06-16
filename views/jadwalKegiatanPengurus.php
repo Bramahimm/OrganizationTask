2e10dc03ff6fed53eb187c47c9c3d341b1c2ae6d
Bramahimsa <bramahimsa123@gmail.com>
2025-06-17 00:27:18 +0700

    nyoba branch lagi

--- views/jadwalKegiatanPengurus.php
+++ views/jadwalKegiatanPengurus.php
@@ -66,7 +66,7 @@ if (isset($_GET['hapus'])) {
         $messageType = "error";
     }
 }
-brnabrabra
+
 // READ - Ambil data kegiatan
 $query = "SELECT * FROM kegiatan ORDER BY tanggal ASC";
 $result = mysqli_query($conn, $query);
